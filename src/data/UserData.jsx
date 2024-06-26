64033d1be8c9fe5605e57cbd042f231a91fa60f0
Asaf Zafrir <asafza@jfrog.com>
2024-06-26 14:29:42 +0300

    start project

--- src/data/UserData.jsx
+++ src/data/UserData.jsx
@@ -1,27 +1,27 @@
 export const UserData = {
-  name: "Rahul Vijay",
+  name: "Asaf Zafrir",
   about : "I'm a front end developer that learned everything on my own. I specialize in creating dynamic, accessible websites that work on any device. Furthermore, I regularly create creative solutions that improve user experiences. I am proficient in React.js, Redux, JavaScript, HTML5, and CSS. I guarantee efficient code management and communication thanks to my knowledge with Git version control. Likewise, I'm also skilled at developing apps using mini program studio frameworks and exceeding client expectations with my high-caliber solutions.",
-  resumeUrl : "https://drive.google.com/file/d/1LSVLVmJA_3fhWtZPc_d_pHnkXhL4s-Oc/view",
-  FooterLink : "https://www.linkedin.com/in/rahulvijay81/",
+  resumeUrl : "https://drive.google.com/file/d/1LSVLVmJA_3fhWtZPc_d_pHnkXhL4s-Oc/view", // change
+  FooterLink : "https://www.linkedin.com/in/asaf-zafrir/",
   socialMedia: [
     {
       socialMediaName: "github",
-      url: "https://github.com/rahulvijay81",
+      url: "https://github.com/asafzaf",
       icon: "AiFillGithub",
     },
     {
       socialMediaName: "linkedin",
-      url: "https://www.linkedin.com/in/rahulvijay81/",
+      url: "https://www.linkedin.com/in/asaf-zafrir/",
       icon: "FaLinkedinIn",
     },
     {
       socialMediaName: "twitter",
       url: "https://twitter.com/rahulvijay8156",
       icon: "AiOutlineTwitter",
-    },
+    }, // to change
     {
       socialMediaName: "instagram",
-      url: "https://instagram.com/rahulvijay81",
+      url: "https://www.instagram.com/asaf_z",
       icon: "AiFillInstagram",
     },
   ],
@@ -32,6 +32,7 @@ export const UserData = {
       "React Native Developer",
       "UI Developer",
       "Javascript Developer",
+      "Web&Cloud Developer"
     ],
     autoStart: true,
     loop: true,
